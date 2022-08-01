60dd6569ac0bd2226e4c8735f111b23ccff47089
fredvdk <36589184+fredvdk@users.noreply.github.com>
2022-08-01 16:38:15 +0200

    1 Aug release

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hero from './hero';
+import './bootstrap.min.css';
+import MainPage from './mainpage';
+import Navbar from './nav';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import ReservationForm from './reservationFrom';
+import SuccessForm from './SuccessForm';
+
 
 function App() {
   return (
+   <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<MainPage />}/>
+        <Route path='/reservatie' element={<ReservationForm />}/>
+        <Route path='/success' element={<SuccessForm />}/>
+      </Routes>
+    
     </div>
+    </Router>
+    
   );
 }
 
